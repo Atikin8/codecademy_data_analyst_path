d84d7095a55281d87f9cea7c481cd8ea84f7505e
Nikita <69020374+Atikin8@users.noreply.github.com>
2021-01-16 11:36:24 -0500

    Update sql_NewYork_restaurants.sql

--- sql_NewYork_restaurants.sql
+++ sql_NewYork_restaurants.sql
@@ -1,4 +1,8 @@
-  SELECT * FROM nomnom; 
+ /* We have put together a table of restaurants called nomnom and we need your help to answer some questions. 
+ Use the SQL commands you just learned and find the best dinner spots in the city.  */
+ 
+ 
+ SELECT * FROM nomnom; 
 
   SELECT DISTINCT neighborhood FROM nomnom; 
 
